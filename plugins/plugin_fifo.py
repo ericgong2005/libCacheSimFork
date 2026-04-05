a6628bbef3a24866dc12d05ad8ed1337c9175c8b
Eric Gong <ericgong2005@gmail.com>
2026-04-04 21:33:26 -0400

    naming

--- plugins/plugin_fifo.py
+++ plugins/plugin_fifo.py
@@ -8,7 +8,7 @@ class FifoCache:
         self.cache_size = cache_size
 
     def on_hit(self, req: Request):
-        pass  # FIFO doesn't reorder on hit
+        pass  # FIFO does not reorder on hit
 
     def on_miss(self, req: Request):
         if req.obj_size <= self.cache_size:
@@ -23,43 +23,45 @@ class FifoCache:
         try:
             self.queue.remove(obj_id)
         except ValueError:
-            pass  # Object not in queue
+            pass
 
 
-def cache_init_hook(common_cache_params: CommonCacheParams):
+def init_hook(common_cache_params: CommonCacheParams):
     return FifoCache(common_cache_params.cache_size)
 
 
-def cache_hit_hook(data: FifoCache, req: Request):
+def hit_hook(data: FifoCache, req: Request):
     data.on_hit(req)
 
 
-def cache_miss_hook(data: FifoCache, req: Request):
+def miss_hook(data: FifoCache, req: Request):
     data.on_miss(req)
 
 
-def cache_eviction_hook(data: FifoCache, req: Request):
+def eviction_hook(data: FifoCache, req: Request):
     return data.evict(req)
 
 
-def cache_remove_hook(data: FifoCache, obj_id: int):
+def remove_hook(data: FifoCache, obj_id: int):
     data.on_remove(obj_id)
 
 
-def cache_free_hook(data: FifoCache):
+def free_hook(data: FifoCache):
     data.queue.clear()
+
+
 if __name__ == "__main__":
     from pathlib import Path
     from libcachesim import PluginCache, TraceReader, TraceType
 
     plugin_fifo_cache = PluginCache(
-        cache_size=1024 * 1024,  # 1 MB
-        cache_init_hook=cache_init_hook,
-        cache_hit_hook=cache_hit_hook,
-        cache_miss_hook=cache_miss_hook,
-        cache_eviction_hook=cache_eviction_hook,
-        cache_remove_hook=cache_remove_hook,
-        cache_free_hook=cache_free_hook,
+        cache_size=1024 * 1024,
+        cache_init_hook=init_hook,
+        cache_hit_hook=hit_hook,
+        cache_miss_hook=miss_hook,
+        cache_eviction_hook=eviction_hook,
+        cache_remove_hook=remove_hook,
+        cache_free_hook=free_hook,
         cache_name="fifo",
     )
 
