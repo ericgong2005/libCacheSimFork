6fbaf1a3ee4134e135cb6eb363aa013007224f65
Eric Gong <ericgong2005@gmail.com>
2026-04-08 13:47:02 -0400

    test

--- plugins/builtin_test.py
+++ plugins/builtin_test.py
@@ -33,3 +33,28 @@ def eviction_hook(data, req: Request):
 
 def remove_hook(data, obj_id: int):
     data.remove(obj_id)
+
+def free_hook(data):
+    pass
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from libcachesim import PluginCache, TraceReader, TraceType
+
+    plugin_fifo_cache = PluginCache(
+        cache_size=1024 * 1024,
+        cache_init_hook=init_hook,
+        cache_hit_hook=hit_hook,
+        cache_miss_hook=miss_hook,
+        cache_eviction_hook=eviction_hook,
+        cache_remove_hook=remove_hook,
+        cache_free_hook=free_hook,
+        cache_name="fifo",
+    )
+
+    trace = Path(__file__).parent.parent / "data" / "cloudPhysicsIO.vscsi"
+    reader = TraceReader(trace=str(trace), trace_type=TraceType.VSCSI_TRACE)
+
+    req_miss_ratio, byte_miss_ratio = plugin_fifo_cache.process_trace(reader)
+    print(f"Request miss ratio: {req_miss_ratio:.4f}")
+    print(f"Byte miss ratio: {byte_miss_ratio:.4f}")
